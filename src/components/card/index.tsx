90aee37b82b666ace44c1c92cba8bdc8514ef0b6
xueqian <weijiagou@gmail.com>
2020-11-29 19:17:13 +0800

    feat: UI Done

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,20 +1,31 @@
+import { url } from 'inspector'
 import React, { Component } from 'react'
-
+import './index.scss'
 interface Props {
-  
+
 }
 interface State {
-  
+
 }
 
-export default class Card extends Component<Props, State> {
-  state = {}
+// export default class Card extends Component<Props, State> {
+export default class Card extends Component<any, any> {
+  constructor(props) {
+    super(props)
+    this.state = {
 
+    }
+  }
+
+  // require('./../body/imgs/11.png')
   render() {
+    const { style, className } = this.props
+
     return (
-      <div>
-        
+      <div className={`card ${className}`} style={{ ...style }}>
+        {this.props.children}
       </div>
+
     )
   }
 }
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import './index.scss'
 interface Props {
-  
+
 }
 interface State {
-  
+
 }
 
 export default class Footer extends Component<Props, State> {
@@ -13,7 +13,26 @@ export default class Footer extends Component<Props, State> {
   render() {
     return (
       <footer className="footer">
-        footer
+        <div className="container">
+          <div className="logo"></div>
+          <div className="contact">
+            <ul>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Help Centre</a></li>
+              <li><a href="#">The Group</a></li>
+              <li><a href="#">Affiliates</a></li>
+            </ul>
+          </div>
+          <div className="info">
+            <ul>
+              <li><a href="#">Information</a></li>
+              <li><a href="#">Term & Condition</a></li>
+              <li><a href="#">Payment methods</a></li>
+              <li><a href="#">Bonus Terms</a></li>
+              <li><a href="#">Responsible Gaming</a></li>
+            </ul>
+          </div>
+        </div>
       </footer>
     )
   }
